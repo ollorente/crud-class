15cddc03b4ff3eccdd4bdb062f164c551aaa7933
Oscar Llorente <llorente_oscar@hotmail.com>
2019-10-23 12:47:44 -0500

    Ajustadas rutas

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-const _URL = 'https://jsonplaceholder.typicode.com/posts'
+const _URL = 'https://jsonplaceholder.typicode.com'
 
 const app = document.getElementById('app')
-const button = document.getElementById('button')
+const createClick = document.getElementById('createClick')
 const ListClick = document.getElementById('listClick')
 const GetOneClick = document.getElementById('getOneClick')
-const UpadteClick = document.getElementById('upadteClick')
+const UpdateClick = document.getElementById('updateClick')
 const RemoveClick = document.getElementById('removeClick')
 
-button.addEventListener('click', () => {
+createClick.addEventListener('click', (e) => {
+    e.preventDefault()
+
     const newPost = {
         userId: 1,
         id: 1,
         title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
         body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
     }
-
-    console.log(newPost)
-    console.log(JSON.stringify(newPost))
 
     fetch(`${_URL}/posts`, {
         method: 'POST',
@@ -27,101 +26,79 @@ button.addEventListener('click', () => {
     .then(data => {
         console.log(data)
     })
-    .catch(e) {
+    .catch(e => {
         console.error(e)
-    }
+    })
 })
 
-ListClick.addEventListener('click', listData())
-GetOneClick.addEventListener('click', getOneData())
-UpadteClick.addEventListener('click', upadteData())
-RemoveClick.addEventListener('click', removeData())
+ListClick.addEventListener('click', (e) => {
+    e.preventDefault()
 
-/* function createData () {
+    fetch(`${_URL}/posts`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(e => {
+        console.error(e)
+    })
+})
+
+GetOneClick.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    fetch(`${_URL}/posts/1`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(e => {
+        console.error(e)
+    })
+})
+
+UpdateClick.addEventListener('click', (e) => {
+    e.preventDefault()
+
     const newPost = {
         userId: 1,
         id: 1,
-        title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, quae.",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aspernatur laudantium esse repellat, cupiditate, nihil illum ducimus iusto voluptatem, explicabo distinctio molestias sed ut velit aliquam ipsam similique animi quam."
     }
-console.log(newPost)
-    fetch(`${_URL}/posts`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    })
-    .then(res => res.jason())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(e) {
-        console.error(e)
-    }
-} */
 
-/* function listData () {
-    console.log('diste clic en List')
-    fetch(`${_URL}/posts`)
-    .then(res => res.jason())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(e) {
-        console.error(e)
-    }
-} */
-
-/* function getData () {
-    fetch(`${_URL}/posts/1`)
-    .then(res => res.jason())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(e) {
-        console.error(e)
-    }
-} */
-
-/* function updateData () {
     fetch(`${_URL}/posts/1`, {
         method: 'PUT',
-        headers: { 'Content-Type': 'application/json' }
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newPost)
     })
-    .then(res => res.jason())
+    .then(res => res.json())
     .then(data => {
         console.log(data)
     })
-    .catch(e) {
+    .catch(e => {
         console.error(e)
-    }
-} */
+    })
+})
 
-/* function removeData () {
+RemoveClick.addEventListener('click', (e) => {
+    e.preventDefault()
+
     fetch(`${_URL}/posts/1`, {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' }
     })
-    .then(res => res.jason())
+    .then(res => res.json())
     .then(data => {
         console.log(data)
     })
-    .catch(e) {
+    .catch(e => {
         console.error(e)
-    }
-} */
-
-/* class GetData {
-
-    constructor () {}
-
-    getData () {
-        fetch(`${_URL}/posts/1`)
-        .then(res => res.jason())
-        .then(data => {
-            console.log(data)
-        })
-        .catch(e) {
-            console.error(e)
-        }
-    }
-
-} */
+    })
+})
